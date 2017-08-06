8d6365775877607b78b45d81f85269dbb3fd1c4e
Muhammad Iqbal Hussain <iqbal.hussain@gaditek.com>
2017-08-06 11:00:01 -0700

    initial commit of angular + laravel

---
+++ angular_app/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+        Working Angular + Laravel
+    `
+})
+export class AppComponent {}
---
+++ public/main.app.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var platform_browser_dynamic_1 = require("@angular/platform-browser-dynamic");
+var core_1 = require("@angular/core");
+var app_environment_js_1 = require("./environments/app.environment.js");
+var app_module_1 = require("./app/app.module");
+if (app_environment_js_1.environment.production) {
+    core_1.enableProdMode();
+}
+platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
